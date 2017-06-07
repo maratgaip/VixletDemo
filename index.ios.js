73fbbb5dd57499b420fab400c8aea62e15b90d9b
maratgaip <marattig@gmail.com>
2017-06-07 10:51:07 -0700

    changed test code

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Inbox from './app/components/inbox';
-import { AppRegistry } from 'react-native';
+import DirectMessage from '@vixlet/react-native-direct-message'
+import {
+  AppRegistry,
+  Text,
+  View
+} from 'react-native';
 
 export default class VixletDemo extends Component {
   render() {
-    return <Inbox/>
+    var allProps = [];
+    for (var prop in this.props) {
+      allProps.push(<View><Text>{prop +':' + obj[prop]}</Text></View>);
+    }
+    return <View>{ allProps }</View>
   }
 }
 
